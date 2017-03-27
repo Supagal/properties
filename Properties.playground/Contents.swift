88d39ad51472ad5f88672cd8effb1c834fee9d63
Supagal <lgrif57@gmail.com>
2017-03-27 08:41:53 -0400

    Add files via upload

---
+++ Properties.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+struct Person {
+    var clothes: String {
+        willSet {
+            updateUI(msg: "I'm changing from \(clothes) to \(newValue)")
+        }
+        
+        didSet {
+            updateUI(msg: "I just changed from \(oldValue) to \(clothes)")
+        }
+    }
+}
+
+func updateUI(msg: String) {
+    print(msg)
+}
+
+var taylor = Person(clothes: "T-shirts")
+taylor.clothes = "short skirts"
+
+
